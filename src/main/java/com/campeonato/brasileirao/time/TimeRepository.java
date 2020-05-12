65bd6151198a92aaaaeb3476bf69aa2ac47fc2f0
bugorin <caiobugorinml@gmail.com>
2020-05-12 10:48:58 -0300

    adicionando logica para o brasileirao

--- src/main/java/com/campeonato/brasileirao/time/TimeRepository.java
+++ src/main/java/com/campeonato/brasileirao/time/TimeRepository.java
@@ -1,7 +1,12 @@
 package com.campeonato.brasileirao.time;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
 
 public interface TimeRepository extends JpaRepository<Time, Long> {
+
+    @Query("SELECT t.nome as nome, t.estado as estado FROM Time t WHERE t.id = 2 ")
+    List<TimeNome> findAllBy();
 
 }
